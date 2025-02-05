e25409ae4adfb12b28ab43c2e72e6388c8aa99a3
Kristy Thompson <kristy.h.thompson@gmail.com>
2025-02-04 20:03:22 -0700

    fixing signup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'node:path';
 import mongoose from 'mongoose';
+import cors from 'cors'; // Import cors package
 import routes from './routes/api/index.js';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
@@ -17,8 +18,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = 'mongodb+srv://kristynhayes:pTbo5YcUGuj6nOvP@codingbootcamp.htygz.mongodb.net/googlebooks?retryWrites=true&w=majority&appName=CodingBootCamp';
 
+// CORS configuration
+const corsOptions = {
+  origin: process.env.NODE_ENV === 'production' ? 'https://bookdragon.onrender.com' : ' http://localhost:10000', // Replace with your actual frontend URL
+  credentials: true,  // Allows cookies to be sent with requests if needed
+};
 
 // Middleware
+app.use(cors(corsOptions)); // Enable CORS for your frontend
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -28,37 +35,35 @@ const server = new ApolloServer({
   resolvers,
 });
 
-
 await server.start();
-console.log('Apollo Server started'); 
+console.log('Apollo Server started');
 
 app.use(
   '/graphql',
   expressMiddleware(server, {
     context: async ({ req }) => {
       const token = req.headers.authorization?.split(' ')[1] || '';
-      const user = getUserFromToken(token); 
+      const user = getUserFromToken(token);
       return { user };
     },
   })
 );
 
-
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../../client/dist')));
   app.get('*', (_, res) => {
     res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
-});
+  });
 }
 
 app.use(routes);
-console.log('Routes added...'); 7
+console.log('Routes added...');
 console.log(MONGO_URI);
 
 mongoose.connect(MONGO_URI);
 
 mongoose.connection.on('connected', () => {
-  console.log('MongoDB connected successfully'); 
+  console.log('MongoDB connected successfully');
 
   app.listen(PORT, () => console.log(`🌍 Now listening on http://localhost:${PORT}`));
 });
@@ -70,4 +75,3 @@ mongoose.connection.on('error', (err) => {
 mongoose.connection.on('disconnected', () => {
   console.log('MongoDB disconnected.');
 });
-
