652fcce54d1fde6fd1cc70f2147e538e2ea0db2f
Kristy Thompson <kristy.h.thompson@gmail.com>
2025-02-06 18:35:11 -0700

    try again

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -1,59 +1,55 @@
-import jwt from 'jsonwebtoken';
+import { AuthenticationError } from 'apollo-server-errors';
 import User from '../models/User.js';
-import Book from '../models/Book.js'; // Importing Book model
+import { signToken } from '../services/auth.js';
 
 const resolvers = {
   Query: {
-    me: async (_: any, __: any, { user }: any) => {
-      const loggedinUser = await User.findOne({ email: user.email });
-      if (!loggedinUser) throw new Error('You need to be logged in!');
-      return loggedinUser; // `user` comes from the authMiddleware
-    },
-    books: async () => {
-      return await Book.find(); // Fetch all books from the database
+    me: async (_parent: unknown, _args: unknown, context: { user: any }) => {
+      if (!context.user) {
+        throw new AuthenticationError('You must be logged in!');
+      }
+      return User.findById(context.user._id).populate('savedBooks');
     },
   },
   Mutation: {
-    login: async (_: any, { email, password }: any) => {
+    login: async (_parent: unknown, { email, password }: { email: string; password: string }) => {
       const user = await User.findOne({ email });
-      if (!user || !(await user.isCorrectPassword(password))) {
-        throw new Error('Incorrect credentials');
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
       }
-      const token = jwt.sign({ userId: user._id }, 'your-secret-key', { expiresIn: '1h' });
+      const isPasswordValid = await user.isCorrectPassword(password);
+      if (!isPasswordValid) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+      const token = signToken(user._id.toString(), user.email, user.username);
       return { token, user };
     },
-    addUser: async (_: any, { username, email, password }: any) => {
+    addUser: async (_parent: unknown, { username, email, password }: { username: string; email: string; password: string }) => {
       const user = await User.create({ username, email, password });
-      const token = jwt.sign({ userId: user._id }, 'your-secret-key', { expiresIn: '1h' });
+      const token = signToken(user._id.toString(), user.email, user.username);
       return { token, user };
     },
-    saveBook: async (_: any, { bookId, authors, description, title, image, link }: any, { user }: any) => {
-      console.log(user);
-      const loggedinUser = await User.findOne({ email: user.email });
-      if (!loggedinUser) throw new Error('You need to be logged in!');
-
-      // Create a new Book instance using the imported Book model
-      const book = new Book({
-        bookId,
-        authors,
-        description,
-        title,
-        image,
-        link,
-      });
-
-      // Push the Book instance into the savedBooks array
-      loggedinUser.savedBooks.push(book);
-      await loggedinUser.save();
-
-      return loggedinUser;
+    saveBook: async (_parent: unknown, { bookData }: { bookData: any }, context: { user: any }) => {
+      if (!context.user) {
+        throw new AuthenticationError('You must be logged in!');
+      }
+      const updatedUser = await User.findByIdAndUpdate(
+        context.user._id,
+        { $addToSet: { savedBooks: bookData } },
+        { new: true, runValidators: true }
+      ).populate('savedBooks');
+      return updatedUser;
     },
-    removeBook: async (_: any, { bookId }: any, { user }: any) => {
-      const loggedinUser = await User.findOne({ email: user.email });
-      if (!loggedinUser) throw new Error('You need to be logged in!');
-      loggedinUser.savedBooks = loggedinUser.savedBooks.filter((book: any) => book.bookId !== bookId);
-      await loggedinUser.save();
-      return loggedinUser;
+    removeBook: async (_parent: unknown, { bookId }: { bookId: string }, context: { user: any }) => {
+      if (!context.user) {
+        throw new AuthenticationError('You must be logged in!');
+      }
+      const updatedUser = await User.findByIdAndUpdate(
+        context.user._id,
+        { $pull: { savedBooks: { bookId } } },
+        { new: true }
+      );
+      return updatedUser;
     },
   },
 };
